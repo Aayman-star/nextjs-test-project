b80cb9d8a088385ed717c7ef2944d8620dfd376c
Aayman-star <aayman.khalid1@gmail.com>
2024-02-27 19:29:10 +0500

    first commit

---
+++ app/ui/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div className="grid h-screen w-full place-content-center">
+      <h1 className="text-2xl font-bold text-gray-900">
+        This is the Login page
+      </h1>
+    </div>
+  );
+};
+
+export default Page;
